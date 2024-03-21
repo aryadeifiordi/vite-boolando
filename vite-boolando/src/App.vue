885c8e6c66e9bdc3efba9be44f702547b1e43816
aryadeifiordi <arya.93.ar@gmail.com>
2024-03-21 17:56:03 +0100

    export default

--- vite-boolando/src/App.vue
+++ vite-boolando/src/App.vue
@@ -1,14 +1,21 @@
-<script setup>
-import AppHeader from './components/AppHeader.vue';
-
-</script>
-
 <template>
   <div>
     <AppHeader />
+    <Card />
   </div>
-  
 </template>
 
+<script>
+import AppHeader from './components/AppHeader.vue';
+import Card from './components/Card.vue';
+export default {
+  components: {
+    AppHeader,
+    Card,
+  }
+}
+</script>
+
 <style scoped>
+
 </style>
--- vite-boolando/src/components/AppHeader.vue
+++ vite-boolando/src/components/AppHeader.vue
@@ -1,54 +1,53 @@
 <template>
-    <header class="header-container">
-      <ul class="choose">
-        <li>Donna</li>
-        <li>Uomo</li>
-        <li>Bambini</li>
-      </ul>
-  
-      <div class="boolean">
-        <img class="img-boolean" src="/img/boolean-logo.png" alt="">
-      </div>
-  
-      <div>
-        <i class="fa-solid fa-bag-shopping"></i>
-        <i class="fa-regular fa-user"></i>
-        <i class="fa-regular fa-heart"></i>
-      </div>
-    </header>
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style scoped>
-  .header-container {
-    background-color: #ff5b00;
-    width: auto;
-    height: 50px;
-    display: flex;
-    justify-content: space-between;
-  }
-  
-  .choose {
-    list-style-type: none;
-    display: flex;
-    justify-content: space-between;
-    gap: 0.5rem;
-  }
-  
-  .img-boolean {
-    width: 120px;
-  }
-  
-  .boolean {
-    display: flex;
-    align-items: center;
-  }
-  
-  .header-container i {
-    font-size: 30px;
-    color: white;
-  }
-  </style>
+  <header class="header-container">
+    <ul class="choose">
+      <li>Donna</li>
+      <li>Uomo</li>
+      <li>Bambini</li>
+    </ul>
+
+    <div class="boolean">
+      <img class="img-boolean" src="/img/boolean-logo.png" alt="">
+    </div>
+
+    <div>
+      <i class="fa-solid fa-bag-shopping"></i>
+      <i class="fa-regular fa-user"></i>
+      <i class="fa-regular fa-heart"></i>
+    </div>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.header-container {
+  background-color: #ff5b00;
+  width: auto;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.choose {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  gap: 0.5rem;
+}
+
+.img-boolean {
+  width: 120px;
+}
+
+.boolean {
+  display: flex;
+  align-items: center;
+}
+
+.header-container i {
+  font-size: 30px;
+  color: white;
+}
+</style>
