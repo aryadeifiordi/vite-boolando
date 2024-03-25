eb208dc24368e610ccc7613c08dc03869c536e95
aryadeifiordi <arya.93.ar@gmail.com>
2024-03-25 16:02:56 +0100

    productcard.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,68 @@
 <template>
   <div>
     <AppHeader />
-    <Card />
+    <Card :product="product" v-for="product in products" :key="product.id" />
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
 import Card from './components/Card.vue';
+
 export default {
   components: {
     AppHeader,
     Card,
-  }
-}
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: "1",
+          brand: "Levi's",
+          title: "RELAXED FIT TEE UNISEX",
+          imageSrc: "/img/1.webp",
+          discount: "-30%",
+          sustainability: "sostenibilità",
+        },
+        {
+          id: "2",
+          brand: "Guess",
+          title: "Roses Tee",
+          imageSrc: "/img/2.webp",
+          discount: "-30%",
+        },
+        {
+          id: "3",
+          brand: "Come zucchero filato",
+          title: "VOGLIA DI COLORI PASTELLO",
+          imageSrc: "/img/3.webp",
+          discount: "-30%",
+        },
+        {
+          id: "4",
+          brand: "Levi's",
+          title: "TEE UNISEX",
+          imageSrc: "/img/4.webp",
+          discount: "-50%",
+          sustainability: "sostenibilità",
+        },
+        {
+          id: "5",
+          brand: "Maya Deluxe",
+          title: "STRIPE BODICE",
+          imageSrc: "/img/5.webp",
+        },
+        {
+          id: "6",
+          brand: "Esprit",
+          title: "MAGLIONE-BLACK",
+          imageSrc: "/img/6.webp",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="product-card">
+        <div class="product-image">
+            <img :src="product.imageSrc" alt="">
+        </div>
+        <div class="product-content">
+            <h2>{{ product.name }}</h2>
+            <p>{{ product.description }}</p>
+            <div class="product-price">{{ product.price }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        product: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
+
+<style scoped>
+.product-card {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+    margin-bottom: 20px;
+}
+
+.product-image img {
+    width: 100%;
+    border-radius: 5px;
+}
+
+.product-content {
+    padding-top: 10px;
+}
+
+.product-content h2 {
+    font-size: 1.2em;
+}
+
+.product-content p {
+    font-size: 0.8em;
+}
+
+.product-price {
+    font-weight: bold;
+    font-size: 1.1em;
+    color: #007bff;
+}
+</style>
